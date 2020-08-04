662ffe679ae61836647811ec1c69249cd6ea0aaf
Michele Marino <m.marino@reply.it>
2020-08-04 13:58:57 +0200

    first commit

---
+++ src/main/java/it/core/Console.java
@@ -0,0 +1,34 @@
+package it.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Console {
+
+    public static String lineReader() {
+        String lineRead = "";
+        try {
+            BufferedReader toRead = new BufferedReader(new InputStreamReader(System.in));
+            lineRead = toRead.readLine();
+        } catch (IOException e) {
+            System.err.println("no way!");
+        }
+        return lineRead;
+    }
+
+    public static String lineReaderScanner() {
+        String lineRead = "";
+        Scanner toRead = new Scanner(System.in);
+        lineRead = toRead.nextLine();
+        return lineRead;
+    }
+
+    public static int intReader() throws IOException {
+        int intRead = 0;
+        BufferedReader intToRead = new BufferedReader(new InputStreamReader(System.in));
+        return intRead = Integer.parseInt(intToRead.readLine());
+    }
+
+}
---
+++ src/main/java/it/core/anonima.java
@@ -0,0 +1,13 @@
+package it.core;
+
+import java.util.HashMap;
+
+public class anonima extends HashMap<String, String> {
+
+    {
+        put("cane", "CANE");
+    }
+
+
+
+}
